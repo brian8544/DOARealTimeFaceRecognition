a6a55c8bec948b3ff15f5917138b4cf193950f33
Brian Oost <brianleon77@gmail.com>
2023-06-07 00:56:33 +0200

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,8 +7,8 @@
 #include <opencv2/imgproc.hpp>
 
 //#include "Main.h"
-#define CASCADE_FILE_MAIN "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_frontalface_alt2.xml"
-#define CASCADE_FILE_EYES "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_eye_tree_eyeglasses.xml"
+#define CASCADE_FILE_MAIN "D:/Repositories/RealtimeFacialRecognition/contrib/cascades/haarcascade_frontalface_alt2.xml"
+#define CASCADE_FILE_EYES "D:/Repositories/RealtimeFacialRecognition/contrib/cascades/haarcascade_eye_tree_eyeglasses.xml"
 #define IMAGE_DIR "D:/Repositories/RealtimeFacialRecognition/contrib/images"
 
 // Function for face detection
