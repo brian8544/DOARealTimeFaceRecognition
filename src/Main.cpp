74d42e3f76c6f18d2f7b353b01a011e7969e2ae7
Brian Oost <brianleon77@gmail.com>
2023-06-06 21:29:46 +0200

    works

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+#include "Main.h"
+
 using namespace std;
 using namespace cv;
 
@@ -18,34 +20,7 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nest
     // Detect faces in the grayscale image
     vector<Rect> faces;
     cascade.detectMultiScale(gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-    // Iterate over the detected faces
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-        // Draw a rectangle around the face
-        rectangle(img, faces[i], Scalar(255, 0, 0), 2);
-
-        /*
-        // Right now it's very buggy, something takes lots of frames once initiated. Sometimes draws random dots in corners. Probably has to do with sunlight particles.
-        // If a nested cascade has been loaded, then detect eyes within the detected faces
-        if (nestedCascade.empty() == false)
-        {
-            vector<Rect> eyes;
-            nestedCascade.detectMultiScale(gray(faces[i]), eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(10, 10));
-
-            // Iterate over the detected eyes
-            for (size_t j = 0; j < eyes.size(); j++)
-            {
-                // Draw a circle around each eye
-                circle(img, eyes[j].tl(), 5, Scalar(0, 255, 0), -1);
-            }
-        }
-        */
-    }
 }
-
-#define CASCADE_FILE_LOCATION "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_frontalface_alt2.xml"
-#define EYE_CASCADE_FILE_LOCATION "Dd:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_eye_tree_eyeglasses.xml"
 
 int main(int argc, const char** argv)
 {
@@ -62,7 +37,7 @@ int main(int argc, const char** argv)
         system("cls");
         std::cout << "Could not find the face cascade." << std::endl;
         system("pause");
-        return 1;
+        return -1;
     }
     else if (!eyeCascade.load(EYE_CASCADE_FILE_LOCATION)) 
     {
@@ -70,7 +45,7 @@ int main(int argc, const char** argv)
         system("cls");
         std::cout << "Could not find the eye cascade." << std::endl;
         system("pause");
-        return 1;
+        return -1;
     }
 
     // Create a VideoCapture object to capture video from webcam
@@ -79,7 +54,9 @@ int main(int argc, const char** argv)
     // If the webcam is not opened, then exit
     if (!capture.isOpened())
     {
-        cout << "Error opening webcam!" << endl;
+        system("cls");
+        cout << "Error opening webcam." << endl;
+        system("pause");
         return -1;
     }
 
@@ -110,6 +87,11 @@ int main(int argc, const char** argv)
         {
             break;
         }
+        else if (key == 'q')
+        {
+            break;
+        }
+
     }
 
     // Close the VideoCapture object
---
+++ src/Main.h
@@ -0,0 +1,3 @@
+#pragma once
+#define CASCADE_FILE_LOCATION "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_frontalface_alt2.xml"
+#define EYE_CASCADE_FILE_LOCATION "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_eye_tree_eyeglasses.xml"
