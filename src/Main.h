4be647db75ef1876b8394e4d1eac2ebcc54dbd3a
Brian Oost <brianleon77@gmail.com>
2023-06-07 00:35:38 +0200

    .

--- src/Main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-/*
-#define CASCADE_FILE_MAIN "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_frontalface_alt2.xml"
-#define CASCADE_FILE_EYES "D:\\Repositories\\RealtimeFacialRecognition\\contrib\\cascades\\haarcascade_eye_tree_eyeglasses.xml"
-#define IMAGE_DIR "D:/Repositories/RealtimeFacialRecognition/contrib/images"
-*/
