77d2ac514ff63cc89ba27accf8cef6d1d0446ee3
Brian Oost <brianleon77@gmail.com>
2023-06-06 21:35:58 +0200

    Oops.

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,6 +20,29 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nest
     // Detect faces in the grayscale image
     vector<Rect> faces;
     cascade.detectMultiScale(gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+    // Iterate over the detected faces
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        // Draw a rectangle around the face
+        rectangle(img, faces[i], Scalar(255, 0, 0), 2);
+
+        /*
+        // Right now it's very buggy, something takes lots of frames once initiated. Sometimes draws random dots in corners. Probably has to do with sunlight particles.
+        // If a nested cascade has been loaded, then detect eyes within the detected faces
+        if (nestedCascade.empty() == false)
+        {
+            vector<Rect> eyes;
+            nestedCascade.detectMultiScale(gray(faces[i]), eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(10, 10));
+            // Iterate over the detected eyes
+            for (size_t j = 0; j < eyes.size(); j++)
+            {
+                // Draw a circle around each eye
+                circle(img, eyes[j].tl(), 5, Scalar(0, 255, 0), -1);
+            }
+        }
+        */
+    }
 }
 
 int main(int argc, const char** argv)
@@ -91,7 +114,6 @@ int main(int argc, const char** argv)
         {
             break;
         }
-
     }
 
     // Close the VideoCapture object
