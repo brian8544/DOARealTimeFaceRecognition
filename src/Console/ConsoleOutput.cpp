f806655f3ebb7579c4581ce4f563d446dc45c791
Brian Oost <brianleon77@gmail.com>
2023-06-13 22:32:37 +0200

    Fix for multi link colors

--- src/Console/ConsoleOutput.cpp
+++ src/Console/ConsoleOutput.cpp
@@ -1,28 +1,11 @@
+#pragma once
 #include <iostream>
-
-// Color constants
-const std::string RESET_COLOR = "\033[0m";
-const std::string BLACK = "\033[30m";
-const std::string RED = "\033[31m";
-const std::string GREEN = "\033[32m";
-const std::string YELLOW = "\033[33m";
-const std::string BLUE = "\033[34m";
-const std::string MAGENTA = "\033[35m";
-const std::string CYAN = "\033[36m";
-const std::string WHITE = "\033[37m";
-const std::string BRIGHT_BLACK = "\033[90m";
-const std::string BRIGHT_RED = "\033[91m";
-const std::string BRIGHT_GREEN = "\033[92m";
-const std::string BRIGHT_YELLOW = "\033[93m";
-const std::string BRIGHT_BLUE = "\033[94m";
-const std::string BRIGHT_MAGENTA = "\033[95m";
-const std::string BRIGHT_CYAN = "\033[96m";
-const std::string BRIGHT_WHITE = "\033[97m";
+#include "Colors.h"
 
 namespace Messages {
 
     // Function to print the current time inline with std::cout
-    static void printCurrentTime()
+    inline static void printCurrentTime()
     {
         time_t now = time(0);
         struct tm timeInfo;
@@ -32,19 +15,23 @@ namespace Messages {
         std::cout << buffer << " ";
     }
 
-	static void Info(const std::string& text) {
+    /*
+    static void Info(const std::string& text) {
         printCurrentTime();
-        std::cout << GREEN << "[INFO]: " << text << RESET_COLOR << std::endl;
-	}
+        std::cout << GREEN << "[INFO]: " << text << RESET_COLOR << "";
+    }
+    */
+
+    static void Info(const std::string& text) {
+        std::cout << GREEN << "" << text << RESET_COLOR << "";
+    }
 
     static void Notice(const std::string& text) {
-        printCurrentTime();
-        std::cout << YELLOW << "[NOTICE]: " << text << RESET_COLOR << std::endl;
+        std::cout << YELLOW << "" << text << RESET_COLOR << "";
     }
 
     static void Error(const std::string& text) {
-        printCurrentTime();
-        std::cerr << RED << "[ERROR]: " << text << RESET_COLOR << std::endl;
+        std::cout << RED << "" << text << RESET_COLOR << "";
     }
 
 }
